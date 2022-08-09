256dd114864091943ae681961bb466bec6946c90
hunterwr <rog19011@byui.edu>
2022-08-09 13:34:00 -0600

    adding disply to main page

--- Main_page.py
+++ Main_page.py
@@ -2,3 +2,13 @@ import streamlit as st
 import pandas as pd
 import plotnine
 import pymongo
+
+@st.cache
+def get_data():
+    return pd.read_csv("lead_logs.csv")
+df = get_data()
+
+
+# Show a table of the entire dataset.
+st.write("## Our dataset:")
+st.write(df)

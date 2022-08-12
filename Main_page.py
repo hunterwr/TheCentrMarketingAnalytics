a4dae88717cc1e1b37da5e236dd062745ad020f1
hunterwr <rog19011@byui.edu>
2022-08-12 15:15:39 -0600

    slider

--- Main_page.py
+++ Main_page.py
@@ -11,8 +11,8 @@ df = get_data()
 
 
 # Show a table of the entire dataset.
-st.write("## Our dataset:")
-st.write(df)
+#st.write("## Our dataset:")
+#st.write(df)
 
 def init_connection():
     return pymongo.MongoClient(**st.secrets["mongo"])
@@ -34,8 +34,18 @@ for result in results:
 temp_df.columns=temp_df.columns.str.replace('data.','')
 temp_df['AmountSpent'] = pd.to_numeric(temp_df['Amount Spent'])
 
+
+#newdf = df[(df.origin >= "JFK") & (df.carrier == "B6")]
+
 final_df = temp_df
 
 gph = ggplot(data=final_df, mapping=aes(x='Day', y ='AmountSpent', fill='Ad Name')) + geom_bar(stat='identity') + theme_classic() + theme(axis_text_x=element_text(rotation=90, hjust=1))
+
+st.markdown("## Ad Spend Summary")
+
+max = temp_df['Day'].max()
+min = temp_df['Day'].min()
+
+values = st.slider("Select the date range", min_value=None, max_value=None, value=(min, max), step=None, format=None, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
 
 st.pyplot(ggplot.draw(gph))

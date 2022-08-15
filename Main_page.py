6c67763786e6d8e66fea897aa902ae5446313050
hunterwr <rog19011@byui.edu>
2022-08-15 09:49:17 -0600

    Filter df

--- Main_page.py
+++ Main_page.py
@@ -39,11 +39,8 @@ temp_df['AmountSpent'] = pd.to_numeric(temp_df['Amount Spent'])
 
 #newdf = df[(df.origin >= "JFK") & (df.carrier == "B6")]
 
-final_df = temp_df
-
-gph = ggplot(data=final_df, mapping=aes(x='Day', y ='AmountSpent', fill='Ad Name')) + geom_bar(stat='identity') + theme_classic() + theme(axis_text_x=element_text(rotation=90, hjust=1))
-
 st.markdown("## Ad Spend Summary")
+
 temp_df['Datetime'] = pd.to_datetime(temp_df['Day'])
 max_year = temp_df['Datetime'].max().year
 max_month = temp_df['Datetime'].max().month
@@ -51,6 +48,27 @@ max_day = temp_df['Datetime'].max().day
 min_year = temp_df['Datetime'].min().year
 min_month = temp_df['Datetime'].min().month
 min_day = temp_df['Datetime'].min().day
+
+
+
+## Range selector
+
+format = 'MMM DD, YYYY'  # format output
+start_date = dt.date(year=min_year,month=min_month,day=min_day)  #  I need some range in the past
+end_date = dt.date(year=max_year,month=max_month,day=max_day)
+max_days = end_date-start_date
+
+slider = st.slider('Select date', min_value=start_date, value=(start_date, end_date) ,max_value=end_date, format=format)
+
+#greater than the start date and smaller than the end date
+mask = (temp_df['date'] > start_date) & (temp_df['date'] <= end_date)
+
+final_df = temp_df.loc[mask]
+
+gph = ggplot(data=final_df, mapping=aes(x='Day', y ='AmountSpent', fill='Ad Name')) + geom_bar(stat='identity') + theme_classic() + theme(axis_text_x=element_text(rotation=90, hjust=1))
+
+
+
 
 #values = st.slider("Select the date range", min_value=None, max_value=None, value=(min, max), step=None, format=None, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
 
@@ -64,20 +82,6 @@ import pandas as pd
 
 
 
-## Range selector
-
-format = 'MMM DD, YYYY'  # format output
-start_date = dt.date(year=min_year,month=min_month,day=min_day)  #  I need some range in the past
-end_date = dt.date(year=max_year,month=max_month,day=max_day)
-max_days = end_date-start_date
-
-slider = st.slider('Select date', min_value=start_date, value=(start_date, end_date) ,max_value=end_date, format=format)
-## Sanity check
-st.table(pd.DataFrame([[start_date, slider, end_date]],
-                columns=['start',
-                        'selected',
-                        'end'],
-                index=['date']))
 
 
 st.write(min_year)
